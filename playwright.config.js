55bf45baa185d7f1b9cc9499a7015036a07f0b1e
Akarsh Kantimath <akarshkantimath@gmail.com>
2024-06-26 12:49:40 +0530

    configuration changes testDiretory,timeout,expect,reporter and use parameters

--- playwright.config.js
+++ playwright.config.js
@@ -1,83 +1,22 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { devices } = require('@playwright/test');
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
-module.exports = defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  timeout : 30 * 1000,  //30 sec added as a timeout - maximum time one test can run for
+const config = {
+  testDir: './tests',  //testDir - test directory, in ./tests where we have written the tests
+  timeout : 30 * 1000,  //30 sec added as a timeout - maximum time one test can run for, for any reason if the test fail to execute - it will report as a failure post 30 sec
   expect : {    //expect it's for assertions
       timeout : 5000
   },
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+  use : {  //use is a key - whatever we declare here, all the test spec can use them
+    //here we write about what browser to use, when to take screenshots and many more
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    browserName : 'chromium' //browser engines - chromium, firefox, webkit (safari browser engine) 
+
   },
+  
+  };
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
+ module.exports = config; //Config object is being exported
 
