53dc612b47a738d96716a80272b122c77209e73a
Akarsh Kantimath <akarshkantimath@gmail.com>
2024-07-06 13:44:55 +0530

    global timeout 30 sec for each test and element find timeout of 5 sec

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ const { devices } = require('@playwright/test');
 
 const config = {
   testDir: './tests',  //testDir - test directory, in ./tests where we have written the tests
-  timeout : 30 * 1000,  //30 sec added as a timeout - maximum time one test can run for, for any reason if the test fail to execute - it will report as a failure post 30 sec
+  timeout : 30 * 1000,  //30 sec added as a timeout and also global timeout - maximum time one test can run for, for any reason if the test fail to execute - it will report as a failure post 30 sec
   expect : {    //expect it's for assertions
       timeout : 5000
   },
@@ -12,7 +12,7 @@ const config = {
   use : {  //use is a key - whatever we declare here, all the test spec can use them
     //here we write about what browser to use, when to take screenshots and many more
 
-    browserName : 'webkit', //browser engines - chromium, firefox, webkit (safari browser engine) 
+    browserName : 'chromium', //browser engines - chromium, firefox, webkit (safari browser engine) 
 
     //to run the test everytime, we use npx playwright test tests/8.filename.spec.js --headed
     //instead of giving  --headed everytime, it can configured to run in headed mode
