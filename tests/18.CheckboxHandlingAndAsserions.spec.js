0e029a6983846bcf2dc8872eb984e4b07eed7f0c
Akarsh Kantimath <akarshkantimath@gmail.com>
2024-07-09 10:25:31 +0530

    Checkbox element handling and assurtions, .toBeFalsy() and .toBeTruthy()

--- tests/18.CheckboxHandlingAndAsserions.spec.js
+++ tests/18.CheckboxHandlingAndAsserions.spec.js
@@ -1,5 +1,4 @@
 const {test, expect } = require('@playwright/test');
-const { Console } = require('console');
 
 test('Handling of checkbox and assertions to it', async({page}) => {
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
