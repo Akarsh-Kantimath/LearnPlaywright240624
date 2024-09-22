ba6e3340f458a5458063ac74ae632030499bb923
Akarsh Kantimath <akarshkantimath@gmail.com>
2024-09-22 12:09:57 +0530

    43. How to handle & Automate frames with Playwright - Example

--- tests/43.How_to_handle_Automate_frames_with_PlaywrightExample.spec.js
+++ tests/43.How_to_handle_Automate_frames_with_PlaywrightExample.spec.js
@@ -5,6 +5,8 @@ test('handle & Automate frames with playwright', async({page})=> {
     
     const elementHandle = await page.locator('#courses-iframe');
     const isVisible = await elementHandle.isVisible();
-// const framePage = page.frameLocator('#courses-iframe');
-  //  await framePage.locator('a[href*="lifetime-access"] : visible').click();
+    
+    //by identifying a frame in a page by locator and storing it in one of the const variable, we can switch to variable and navigate to the components of the frame.
+    const framePage = page.frameLocator('#courses-iframe');
+    await framePage.locator('a[href*="lifetime-access"] : visible').click();
     });
